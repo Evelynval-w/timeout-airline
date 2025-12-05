e902cfb026a70362a5950948269c57e2ebf82edc
Makuochukwu okoene <makuochukwu.okoene@epita.fr>
2025-12-05 17:41:52 +0100

    Initial project setup with User, Client, Employee entities

---
+++ src/test/java/fr/epita/timeoutairline/TimeoutairlineApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epita.timeoutairline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeoutairlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
