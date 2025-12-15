5dc5ec8642d96686dce419f26ad7fe7d47dc25e2
Makuochukwu okoene <makuochukwu.okoene@epita.fr>
2025-12-15 15:50:11 +0100

    Add all services

---
+++ src/main/java/fr/epita/timeoutairline/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package fr.epita.timeoutairline.service;
+
+public class EmployeeService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/fr/epita/timeoutairline/service/MilesRewardService.java
@@ -0,0 +1,10 @@
+package fr.epita.timeoutairline.service;
+
+public class MilesRewardService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
