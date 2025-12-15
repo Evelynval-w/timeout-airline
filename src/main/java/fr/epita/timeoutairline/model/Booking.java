89f3af265a831da0cc15ef5994cebb96a0b1f318
Makuochukwu okoene <makuochukwu.okoene@epita.fr>
2025-12-15 15:49:02 +0100

    Add Booking and MilesReward entities

---
+++ src/main/java/fr/epita/timeoutairline/model/Booking.java
@@ -0,0 +1,10 @@
+package fr.epita.timeoutairline.model;
+
+public class Booking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/fr/epita/timeoutairline/model/MilesReward.java
@@ -0,0 +1,10 @@
+package fr.epita.timeoutairline.model;
+
+public class MilesReward {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
